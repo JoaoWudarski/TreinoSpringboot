6ea5eb10bd30b9305875e0750790ce361dd87a00
joaowudarski <joaowudarski@gmail.com>
2020-09-03 20:40:16 -0300

    #NEW 9- resource/clientes

---
+++ src/main/java/com/example/demo/controle/Cliente.java
@@ -0,0 +1,9 @@
+package com.example.demo.controle;
+
+public class Cliente {
+
+    public int codigo;
+    public String nome;
+    public String endereco;
+    public double saldo;
+}
--- src/main/java/com/example/demo/controle/MeuControle.java
+++ src/main/java/com/example/demo/controle/MeuControle.java
@@ -1,27 +1,58 @@
 package com.example.demo.controle;
 
+import java.util.Arrays;
+import java.util.List;
+
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class MeuControle {
     
-    @GetMapping("/cliente")
-    public String cliente() {
-        return "xxxx";
+    List <Cliente> clientes;
+
+    @PostConstruct
+    public void criarClientes(){
+        Cliente c1 = new Cliente();
+        Cliente c2 = new Cliente();
+        Cliente c3 = new Cliente();
+
+        c1.codigo = 1;
+        c1.nome = "Jose";
+        c1.endereco = "Rux X, 99";
+        c1.saldo = 100.345;
+
+        c2.codigo = 2;
+        c2.nome = "Maria";
+        c2.endereco = "Rux Y, 92";
+        c2.saldo = 123;
+
+        c3.codigo = 3;
+        c3.nome = "Mario";
+        c3.endereco = "Rux H, 23";
+        c3.saldo = 1230;
+
+        clientes = Arrays.asList(c1, c2, c3);
     }
 
-    @GetMapping("/celular")
-    public String cel() {
-        return "xxxx";
+    @GetMapping("/clientes")
+    public List<Cliente> cliente() {
+        return clientes;
     }
 
 
-    @GetMapping("/cliente/{codigo}")
-    public String Cliente(@PathVariable int codigo){
+    @GetMapping("/clientes/{codigo}")
+    public Cliente Cliente(@PathVariable int codigo){
+        for(Cliente i : clientes){
+            if(i.codigo == codigo){
+                return i;
+            }
+        }
         
-        
-        return "CLIENTES " + codigo;
+        return null;
     }
 }
